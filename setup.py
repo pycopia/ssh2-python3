28ba8a894806e2cfdf91eb6d68e6e99b2d7dfa78
Keith Dart <keith@dartworks.biz>
2020-12-26 22:05:52 -0800

    Package metadata improvements.

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ import platform
 import os
 import sys
 from glob import glob
-
-from _setup_libssh2 import build_ssh2
+from subprocess import check_call
+from shutil import copy2
 
 import versioneer
 from setuptools import setup, find_packages
@@ -13,12 +13,29 @@ from setuptools import setup, find_packages
 from Cython.Distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-ON_RTD = os.environ.get('READTHEDOCS') == 'True'
 
-SYSTEM_LIBSSH2 = bool(os.environ.get('SYSTEM_LIBSSH2', 0)) or ON_RTD
+def build_ssh2():
+    if not os.path.exists('build'):
+        os.mkdir('build')
+    if os.path.exists('build/Makefile'):
+        return
+    os.chdir('build')
+    try:
+        check_call('cmake ../libssh2 -DBUILD_SHARED_LIBS=ON \
+        -DENABLE_ZLIB_COMPRESSION=ON -DENABLE_CRYPT_NONE=ON \
+        -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF \
+        -DENABLE_MAC_NONE=ON -DCRYPTO_BACKEND=OpenSSL',
+                   shell=True, env=os.environ)
+        check_call('cmake --build . --config Release', shell=True, env=os.environ)
+    finally:
+        os.chdir('..')
+
+    for src in glob('build/src/libssh2.so*'):
+        copy2(src, 'ssh2/')
+
 
 # Only build libssh if running a build
-if not SYSTEM_LIBSSH2 and (len(sys.argv) >= 2 and not (
+if (len(sys.argv) >= 2 and not (
         '--help' in sys.argv[1:] or
         sys.argv[1] in (
             '--help-commands', 'egg_info', '--version', 'clean',
@@ -26,19 +43,12 @@ if not SYSTEM_LIBSSH2 and (len(sys.argv) >= 2 and not (
         __name__ == '__main__'):
     build_ssh2()
 
-ON_WINDOWS = platform.system() == 'Windows'
-
 sources = glob('ssh2/*.pyx')
 _arch = platform.architecture()[0][0:2]
-_libs = ['ssh2'] if not ON_WINDOWS else [
-    'Ws2_32', 'libssh2', 'user32',
-    'libcrypto%sMD' % _arch, 'libssl%sMD' % _arch,
-    'zlibstatic',
-]
+_libs = ['ssh2']
 
 _fwd_default = 0
-_comp_args = ["-O3"] if not ON_WINDOWS else None
-_embedded_lib = bool(int(os.environ.get('EMBEDDED_LIB', 1)))
+_comp_args = ["-O3"]
 _have_agent_fwd = bool(int(os.environ.get('HAVE_AGENT_FWD', _fwd_default)))
 
 cython_directives = {
@@ -53,15 +63,15 @@ cython_args = {
     'include_path': ["ssh2"],
     'cython_directives': cython_directives,
     'cython_compile_time_env': {
-        'EMBEDDED_LIB': _embedded_lib,
+        'EMBEDDED_LIB': True,
         'HAVE_AGENT_FWD': _have_agent_fwd,
     }
 }
 
 
-runtime_library_dirs = ["$ORIGIN/."] if not SYSTEM_LIBSSH2 else None
-_lib_dir = os.path.abspath("./build/src") if not SYSTEM_LIBSSH2 else "/usr/local/lib"
-include_dirs = ["libssh2/include"] if ON_RTD or not SYSTEM_LIBSSH2 else ["/usr/local/include"]
+runtime_library_dirs = ["$ORIGIN/."]
+_lib_dir = os.path.abspath("./build/src")
+include_dirs = ["libssh2/include"]
 
 extensions = [
     Extension(sources[i].split('.')[0].replace(os.path.sep, '.'),
@@ -76,11 +86,6 @@ extensions = [
 
 package_data = {'ssh2': ['*.pxd', 'libssh2.so*']}
 
-if ON_WINDOWS:
-    package_data['ssh2'].extend([
-        'libcrypto*.dll', 'libssl*.dll',
-    ])
-
 cmdclass = versioneer.get_cmdclass()
 cmdclass['build_ext'] = build_ext
 
@@ -88,21 +93,24 @@ setup(
     name='ssh2-python3',
     version=versioneer.get_version(),
     cmdclass=cmdclass,
-    tests_require=['pytest'],
-    url='https://github.com/kdart-brt/ssh2-python',
-    license='LGPLv2',
-    author='Panos Kittenis',
-    author_email='22e889d8@opayq.com',
-    description=('Super fast SSH library - bindings for libssh2 and Python 3'),
-    long_description=open('README.rst').read(),
-    long_description_content_type="text/markdown",
     packages=find_packages(
         '.', exclude=('embedded_server', 'embedded_server.*',
                       'tests', 'tests.*',
                       '*.tests', '*.tests.*')),
     zip_safe=False,
     include_package_data=True,
-    platforms='any',
+    tests_require=['pytest'],
+    python_requires='~=3.6',
+    license='LGPLv2',
+    author='Panos Kittenis',
+    author_email='22e889d8@opayq.com',
+    maintainer='Keith Dart',
+    maintainer_email='keith.dart@gmail.com',
+    description='Super fast SSH library - bindings for libssh2 and Python 3',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/pycopia/ssh2-python3',
+    platforms=['linux_x86_64', 'manylinux2014_x86_64 '],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
@@ -123,7 +131,6 @@ setup(
         'Operating System :: POSIX',
         'Operating System :: POSIX :: Linux',
         'Operating System :: POSIX :: BSD',
-        'Operating System :: MacOS :: MacOS X',
     ],
     ext_modules=extensions,
     package_data=package_data,
